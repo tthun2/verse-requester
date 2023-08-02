ea6f4fae95d09b86c921cd3a0f4dd04cb38b2453
tthun2 <timhung2@gmail.com>
2023-08-02 17:01:27 +1200

    What's working: 1. CLI verse lookup 2. Shorthand works 3. If books don't exist or verses don't exist, an error message shows up
    What needs to be done next:
    1. Song of songs can't be found
    2. Multiple verses and verse range

--- bin/index.js
+++ bin/index.js
@@ -9,7 +9,22 @@ Hence, args[2] is the first value
 */
 const args = process.argv;
 
-//console.log("(Book chapter:verse) " + args[2] + " " + chap + ":" + vers);
+var targetBook;
+var chapter_and_verse;
+var targetChapter;
+var targetVerse;
+
+//In case of books with 1, 2 or 3 in front of the book name. E.g. 1 Corinthians
+if (args[2] == 1 || args[2] == 2 || args[2] == 3){
+    targetBook = args[2] + ' ' + args[3];
+    chapter_and_verse = args[4].split(":"); //split chapter and verse by the :
+} else {
+    targetBook = args[2];
+    chapter_and_verse = args[3].split(":"); //split chapter and verse by the :
+}
+
+targetChapter = chapter_and_verse[0];
+targetVerse = chapter_and_verse[1];
 
 const fs = require('fs');
 
@@ -18,18 +33,32 @@ let asv = JSON.parse(rawdata);
 
 var found = false;
 
-asv.verses.every(element => {
-    if (element.book_name == args[2] &&
-        element.chapter == chap &&
-        element.verse == vers) {
-        console.log(args[2] + " " + chap + ":" + vers + " - " + element.text);
+for (i = 0; i < asv.verses.length; i++){
+    if (checkBookName(asv.verses[i].book_name, targetBook) &&
+        asv.verses[i].chapter == targetChapter &&
+        asv.verses[i].verse == targetVerse) {
+        console.log(asv.verses[i].book_name + " " + targetChapter + ":" + targetVerse + " - " + asv.verses[i].text);
         found = true;
+        break;
     }
-});
-
-if (!found) {
-    console.log(args[2] + " " + chap + ":" + vers + ' cannot be found. Please check your spelling and try again.')
 }
 
+if (!found) {
+    console.log(targetBook + " " + targetChapter + ":" + targetVerse + ' cannot be found. Please check your spelling and try again.')
+}
 
+//Check if the name of the book is correctly entered
+//It also accepts shorthand (minimum 3 characters)
+function checkBookName(sourceBook, targetBook){
+    if (sourceBook == targetBook){
+        return true;
+    } else if (targetBook.length >= 3 && sourceBook.includes(targetBook)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//console.log(asv.verses[0]); //This is Genesis 1:1
+//console.log(asv.verses[23145]); //This is Matthew 1:1
 
