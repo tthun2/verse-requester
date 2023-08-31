7f66bbd8212fa98f51557876c56a8208be58c7bb
Jake He <jake.he@nextgen.group>
2023-08-31 20:17:32 +1000

    clean up code

--- src/verse-requester.js
+++ src/verse-requester.js
@@ -1,86 +1,93 @@
-let bible = require('../data/asv.json');
+let bible = require("../data/asv.json");
 
 let verseRequester = (args) => {
-    let {
-        error,
-        targetBook,
-        targetChapter,
-        targetVerse
-    } = splitInput(args);
+  let { error, book, chapter, verse } = parser(args);
 
-    if (error) {
-        console.log("Please enter a book name, chapter and verse. E.g. Matthew 1:1");
-        return "Please enter a book name, chapter and verse. E.g. Matthew 1:1";
-    } else {
-        return getVerseText(targetBook, targetChapter, targetVerse);
-    }
-}
+  if (error) {
+    console.log(
+      "Please enter a book name, chapter and verse. E.g. Matthew 1:1",
+    );
+    return "Please enter a book name, chapter and verse. E.g. Matthew 1:1";
+  } else {
+    return getVerseText(book, chapter, verse);
+  }
+};
 
 //Concat all args to become 1 string
 //Then split the string into book, chapter and verse
-function splitInput(args) {
-    let inputString = "";
-    let error = false;
+function parser(args) {
+  // point-free style
+  const [bookChapter, verse] = args
+    .slice(2) // remove first 2 elements
+    .join(" ") // join all elements into 1 string
+    .split(":") // split into 2 parts, the bookChapter and verse
+    .map((text) => (text.includes(" ") ? text.split(" ") : text));
 
-    for (let i = 2; i < args.length; i++) {
-        inputString += args[i] + " ";
-    }
-
-    const temp = inputString.split(":"); //Get verse first
-    const temp2 = temp[0].split(" "); //Get book and chapter
-
-    let verse = temp[1];
-    let chapter = temp2[temp2.length - 1];
-    let book = temp[0].substring(0, temp[0].length - chapter.length);
-
-    if (!book || !chapter || !verse) {
-        error = true;
-    } else { //everything is defined, so trim any extra spaces
-        book = book.trim();
-        chapter = chapter.trim();
-        verse = verse.trim();
-    }
-
-    //console.log(book + " " + chapter + " " + verse);
-
+  if (!bookChapter || !verse) {
     return {
-        error: error,
-        targetBook: book,
-        targetChapter: chapter,
-        targetVerse: verse
+      error: true,
     };
+  }
+
+  // last element of the array is the chapter
+  const chapter = bookChapter.pop()?.trim();
+  const book = bookChapter.join(" ").trim();
+
+  return {
+    error: !book || !chapter || !verse,
+    book,
+    chapter,
+    verse,
+  };
 }
 
 function getVerseText(targetBook, targetChapter, targetVerse) {
+  let verseObject = bible.verses.find(
+    (item) =>
+      validateBookName(item.book_name, targetBook) &&
+      item.chapter.toString() === targetChapter &&
+      item.verse.toString() === targetVerse,
+  );
 
-    let verseObject = bible.verses.find(item => validateBookName(item.book_name, targetBook) && item.chapter.toString() === targetChapter && item.verse.toString() === targetVerse);
-
-    if (verseObject) {
-        console.log(`${verseObject.book_name} ${targetChapter}:${targetVerse} - ${verseObject.text}`);
-        return (`${verseObject.book_name} ${targetChapter}:${targetVerse} - ${verseObject.text}`);
-    } else {
-        console.log(`${targetBook} ${targetChapter}:${targetVerse} cannot be found. Please check the reference and try again.`)
-        return (`${targetBook} ${targetChapter}:${targetVerse} cannot be found. Please check the reference and try again.`)
-    }
-
+  if (verseObject) {
+    console.log(
+      `${verseObject.book_name} ${targetChapter}:${targetVerse} - ${verseObject.text}`,
+    );
+    return `${verseObject.book_name} ${targetChapter}:${targetVerse} - ${verseObject.text}`;
+  } else {
+    console.log(
+      `${targetBook} ${targetChapter}:${targetVerse} cannot be found. Please check the reference and try again.`,
+    );
+    return `${targetBook} ${targetChapter}:${targetVerse} cannot be found. Please check the reference and try again.`;
+  }
 }
 
 //Check if the name of the book is correctly entered
 //It also accepts shorthand (minimum 3 characters)
+
+const SONG_OF_SONGS = ["song of songs", "song of solomon"];
 function validateBookName(sourceBook, targetBook) {
-    if (sourceBook.toLowerCase() === targetBook.toLowerCase()) {
-        return true;
-    } else if (targetBook.length >= 3 && sourceBook.toLowerCase().includes(targetBook.toLowerCase())) {
-        return true;
-    } else if (sourceBook.toLowerCase() === "song of solomon" || sourceBook.toLowerCase() === "song of songs") {
-        if (targetBook.length >= 3 && "song of songs".includes(targetBook.toLowerCase()) ||
-            targetBook.length >= 3 && "song of solomon".includes(targetBook.toLowerCase()) ||
-            targetBook.length >= 3 && "s.s.".includes(targetBook.toLowerCase())) {
-            return true;
-        }
-    } else {
-        return false;
+  if (sourceBook.toLowerCase() === targetBook.toLowerCase()) {
+    return true;
+  } else if (
+    targetBook.length >= 3 &&
+    sourceBook.toLowerCase().includes(targetBook.toLowerCase())
+  ) {
+    return true;
+  } else if (SONG_OF_SONGS.includes(sourceBook.toLowerCase())) {
+    // son
+    if (
+      (targetBook.length >= 3 &&
+        "song of songs".includes(targetBook.toLowerCase())) ||
+      (targetBook.length >= 3 &&
+        "song of solomon".includes(targetBook.toLowerCase())) ||
+      (targetBook.length >= 3 && "s.s.".includes(targetBook.toLowerCase()))
+    ) {
+      return true;
     }
+  } else {
+    return false;
+  }
 }
 
 module.exports = { verseRequester };
--- test/index.test.js
+++ test/index.test.js
@@ -1,84 +1,92 @@
-import { expect, test } from 'vitest'
-import { verseRequester} from "../src/verse-requester";
-test('Should return the correct verse', () => {
-    [
-        {
-            args: ['node', '', 'Genesis', '1:1'],
-            expected: 'Genesis 1:1 - In the beginning God created the heavens and the earth.'
-        },
-        {
-            args: ['node', '', 'Genesis', '1:2'],
-            expected: 'Genesis 1:2 - And the earth was waste and void; and darkness was upon the face of the deep: and the Spirit of God moved upon the face of the waters.'
-        },
-        {
-            args: ['node', '', 'Matt', '1:1'],
-            expected: 'Matthew 1:1 - The book of the generation of Jesus Christ, the son of David, the son of Abraham.'
-        },
-        {
-            args: ['node', '', '1 Cor', '1:1'],
-            expected: '1 Corinthians 1:1 - Paul, called [to be] an apostle of Jesus Christ through the will of God, and Sosthenes our brother,'
-        },
-        {
-            args: ['node', '', '3 john', '1:1'],
-            expected: '3 John 1:1 - The elder unto Gaius the beloved, whom I love in truth.'
-        },
-        {
-            args: ['node', '', 'song of songs', '1:1'],
-            expected: `Song of Solomon 1:1 - The Song of songs, which is Solomon's.`
-        }
-    ].forEach(({ args, expected }) => {
-        expect(verseRequester(args)).equal(expected)
-    })
+import { expect, test } from "vitest";
+import { verseRequester } from "../src/verse-requester";
+test("Should return the correct verse", () => {
+  [
+    {
+      args: ["node", "", "Genesis", "1:1"],
+      expected:
+        "Genesis 1:1 - In the beginning God created the heavens and the earth.",
+    },
+    {
+      args: ["node", "", "Genesis", "1:2"],
+      expected:
+        "Genesis 1:2 - And the earth was waste and void; and darkness was upon the face of the deep: and the Spirit of God moved upon the face of the waters.",
+    },
+    {
+      args: ["node", "", "Matt", "1:1"],
+      expected:
+        "Matthew 1:1 - The book of the generation of Jesus Christ, the son of David, the son of Abraham.",
+    },
+    {
+      args: ["node", "", "1 Cor", "1:1"],
+      expected:
+        "1 Corinthians 1:1 - Paul, called [to be] an apostle of Jesus Christ through the will of God, and Sosthenes our brother,",
+    },
+    {
+      args: ["node", "", "3 john", "1:1"],
+      expected:
+        "3 John 1:1 - The elder unto Gaius the beloved, whom I love in truth.",
+    },
+    {
+      args: ["node", "", "song of songs", "1:1"],
+      expected: `Song of Solomon 1:1 - The Song of songs, which is Solomon's.`,
+    },
+  ].forEach(({ args, expected }) => {
+    expect(verseRequester(args)).equal(expected);
+  });
 });
 
-
-test('Wrong verse reference return warning', () => {
-    [
-        {
-            args: ['node', '', 'Ge', '1:1'],
-            expected: 'Ge 1:1 cannot be found. Please check the reference and try again.'
-        },
-        {
-            args: ['node', '', 'Genesis', '12111:2'],
-            expected: 'Genesis 12111:2 cannot be found. Please check the reference and try again.'
-        },
-        {
-            args: ['node', '', 'Matt', '1:1444'],
-            expected: 'Matt 1:1444 cannot be found. Please check the reference and try again.'
-        },
-        {
-            args: ['node', '', 'Rev', '23:1'],
-            expected: 'Rev 23:1 cannot be found. Please check the reference and try again.'
-        },
-        {
-            args: ['node', '', 'Matt', '1:1', 'a'],
-            expected: 'Matt 1:1 a cannot be found. Please check the reference and try again.'
-        }
-    ].forEach(({ args, expected }) => {
-        expect(verseRequester(args)).equal(expected)
-    })
+test("Wrong verse reference return warning", () => {
+  [
+    {
+      args: ["node", "", "Ge", "1:1"],
+      expected:
+        "Ge 1:1 cannot be found. Please check the reference and try again.",
+    },
+    {
+      args: ["node", "", "Genesis", "12111:2"],
+      expected:
+        "Genesis 12111:2 cannot be found. Please check the reference and try again.",
+    },
+    {
+      args: ["node", "", "Matt", "1:1444"],
+      expected:
+        "Matt 1:1444 cannot be found. Please check the reference and try again.",
+    },
+    {
+      args: ["node", "", "Rev", "23:1"],
+      expected:
+        "Rev 23:1 cannot be found. Please check the reference and try again.",
+    },
+    {
+      args: ["node", "", "Matt", "1:1", "a"],
+      expected:
+        "Matt 1:1,a cannot be found. Please check the reference and try again.",
+    },
+  ].forEach(({ args, expected }) => {
+    expect(verseRequester(args)).equal(expected);
+  });
 });
 
-
-test('Not enough arguments', () => {
-    [
-        {
-            args: ['node', ''],
-            expected: 'Please enter a book name, chapter and verse. E.g. Matthew 1:1'
-        },
-        {
-            args: ['node', '', 'Genesis'],
-            expected: 'Please enter a book name, chapter and verse. E.g. Matthew 1:1'
-        },
-        {
-            args: ['node', '', '1', 'Kings'],
-            expected: 'Please enter a book name, chapter and verse. E.g. Matthew 1:1'
-        },
-        {
-            args: ['node', '', '1', 'Kings'],
-            expected: 'Please enter a book name, chapter and verse. E.g. Matthew 1:1'
-        }
-    ].forEach(({ args, expected }) => {
-        expect(verseRequester(args)).equal(expected)
-    })
+test("Not enough arguments", () => {
+  [
+    {
+      args: ["node", ""],
+      expected: "Please enter a book name, chapter and verse. E.g. Matthew 1:1",
+    },
+    {
+      args: ["node", "", "Genesis"],
+      expected: "Please enter a book name, chapter and verse. E.g. Matthew 1:1",
+    },
+    {
+      args: ["node", "", "1", "Kings"],
+      expected: "Please enter a book name, chapter and verse. E.g. Matthew 1:1",
+    },
+    {
+      args: ["node", "", "1", "Kings"],
+      expected: "Please enter a book name, chapter and verse. E.g. Matthew 1:1",
+    },
+  ].forEach(({ args, expected }) => {
+    expect(verseRequester(args)).equal(expected);
+  });
 });
